7b527f98461f409f218fb932954e3d99dd1f70d7
Camille Moyon <camille@taotesting.com>
2020-01-29 11:06:05 +0100

    Bump version

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return [
     'label' => 'extension-tao-testqti-previewer',
     'description' => 'extension that provides QTI test previewer',
     'license'     => 'GPL-2.0',
-    'version' => '2.10.1',
+    'version' => '2.10.2',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'tao'          => '>=37.6.0',
