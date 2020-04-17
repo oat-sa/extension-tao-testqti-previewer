eb73facc76a33d465549d9f73c868bc70b0e616d
Anshul Sharma <ansul@taotesting.com>
2020-04-17 16:40:07 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -27,7 +27,7 @@ return [
     'label' => 'extension-tao-testqti-previewer',
     'description' => 'extension that provides QTI test previewer',
     'license'     => 'GPL-2.0',
-    'version' => '2.12.1',
+    'version' => '2.13.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
         'generis'      => '>=12.15.0',
