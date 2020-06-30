1387444b5ec4e392326311a5d69d70f59ac04e21
Hanna Dzmitryieva <bziondik@gmail.com>
2020-06-30 19:19:06 +0300

    add test preview adapter, proxy, component

---
+++ views/js/previewer/proxy/test.js
@@ -0,0 +1,93 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+
+/**
+ * Test runner proxy for the QTI item previewer
+ *
+ * @author Hanna Dzmitryieva <hanna@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'i18n',
+    'core/promiseQueue',
+    'core/request',
+    'util/url',
+], function ($, _, __, promiseQueue, request, urlUtil) {
+    'use strict';
+
+    const serviceControllerInit = 'TestPreviewer';
+    const serviceControllerGetItem = 'Previewer';
+
+    const serviceExtension = 'taoQtiTestPreviewer';
+    /**
+     * QTI proxy definition
+     * Related to remote services calls
+     * @type {Object}
+     */
+    return {
+        name: 'qtiTestPreviewerProxy',
+
+        /**
+         * Installs the proxy
+         */
+        install: function install() {
+            /**
+             * A promise queue to ensure requests run sequentially
+             */
+            this.queue = promiseQueue();
+        },
+        /**
+         * Initializes the proxy
+         * @returns {Promise} - Returns a promise. The proxy will be fully initialized on resolve.
+         *                      Any error will be provided if rejected.
+         */
+        init: function init() {
+            return request( {
+                url: urlUtil.route('init', serviceControllerInit, serviceExtension)
+            })
+            .then(response => response.data);
+        },
+
+        /**
+         * Uninstalls the proxy
+         * @returns {Promise} - Returns a promise. The proxy will be fully uninstalled on resolve.
+         *                      Any error will be provided if rejected.
+         */
+        destroy: function destroy() {
+            // no request, just a resources cleaning
+            this.queue = null;
+
+            // the method must return a promise
+            return Promise.resolve();
+        },
+
+        /**
+         * Gets an item definition by its identifier, also gets its current state
+         * @param {String} itemUri - The identifier of the item to get
+         * @returns {Promise} - Returns a promise. The item data will be provided on resolve.
+         *                      Any error will be provided if rejected.
+         */
+        getItem: function getItem(itemUri) {
+            return request({
+                url: urlUtil.route('getItem', serviceControllerGetItem, serviceExtension),
+                data: { serviceCallId: 'previewer', itemUri }
+            });
+        }
+    };
+});
