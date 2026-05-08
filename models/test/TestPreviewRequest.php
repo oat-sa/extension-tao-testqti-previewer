d6ec98295f5430bd1e3b0d55d7d33720b673a266
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2026-05-08 15:17:26 +0200

    fix: nullable TestPreviewRequest constructor argument type

--- models/test/TestPreviewRequest.php
+++ models/test/TestPreviewRequest.php
@@ -8,14 +8,14 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
  *
- * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2020-2026 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
@@ -32,7 +32,7 @@ class TestPreviewRequest
     public function __construct(
         string $testUri,
         TestPreviewConfig $config = null,
-        bool $isTimeConstraintRequired = false
+        ?bool $isTimeConstraintRequired = false
     ) {
         $this->testUri = $testUri;
         $this->config = $config ?? new TestPreviewConfig();
